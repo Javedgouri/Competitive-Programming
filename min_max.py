6e5c174f59d6da976eaec506a5fbb52470f41218
JavedGouri <javeda3740@gmail.com>
2018-07-24 02:05:10 +0530

    solved problem

---
+++ min_max.py
@@ -0,0 +1,28 @@
+'''
+Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+Input Format
+
+A single line of five space-separated integers.
+
+Constraints
+
+    Each integer is in the inclusive range .
+
+Output Format
+
+Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than a 32 bit integer.)
+
+Sample Input
+
+1 2 3 4 5
+
+Sample Output
+
+10 14
+
+'''
+n = [int(x) for x in input().strip().split(' ')]
+minsum = sum(n)-max(n)
+maxsum = sum(n)-min(n)
+print(minsum,maxsum)
